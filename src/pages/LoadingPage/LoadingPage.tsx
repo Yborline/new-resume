9b368ba41acadc6110ec6abfbfae5d6cf749b240
Yaroslav Borys <borrline@gmail.com>
2024-11-04 16:47:39 +0300

    added page LoadingPage and styles

---
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./LoadingPage.module.scss";
+import { Atom } from "react-loading-indicators";
+
+const LodingPage = () => {
+  return (
+    <div className={styles.loading}>
+      <Atom color="black" size="large" />
+    </div>
+  );
+};
+
+export default LodingPage;
